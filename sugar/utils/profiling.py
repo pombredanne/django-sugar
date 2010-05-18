dd8935e209790bb664ba5d73b2e3826cc714ef29
Chris Adams <chris@improbable.org>
2010-05-18 15:40:28 -0400

    query_count() can now use inspect to identify anonymous callers

--- sugar/utils/profiling.py
+++ sugar/utils/profiling.py
@@ -3,6 +3,7 @@ Utilities to help localize performance problems
 """
 
 from contextlib import contextmanager
+import inspect
 import time
 import sys
 
@@ -11,7 +12,7 @@ from django.db import connection
 __all__ = ['query_count']
 
 @contextmanager
-def query_count(desc):
+def query_count(desc=None):
     """
     Display a list of the database query count delta for an arbitrary block of
     code enclosed in a with statement::
@@ -23,6 +24,9 @@ def query_count(desc):
 
     retrieve user contact preferences: 37 queries in 0.53 seconds
     """
+
+    if not desc:
+        desc = "[%s:%s]" % inspect.stack()[2][1:3]
 
     # TODO: Verbose mode which displays queries?
     original_query_count = len(connection.queries)
