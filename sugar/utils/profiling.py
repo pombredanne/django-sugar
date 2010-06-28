398cdf5a6def9352931d39606a114cd2208c393d
Chris Adams <chris@improbable.org>
2010-06-28 15:36:01 -0400

    profiling: query_count can optionally print the actual queries

--- sugar/utils/profiling.py
+++ sugar/utils/profiling.py
@@ -12,7 +12,7 @@ from django.db import connection
 __all__ = ['query_count']
 
 @contextmanager
-def query_count(desc=None):
+def query_count(desc=None, print_queries=False):
     """
     Display a list of the database query count delta for an arbitrary block of
     code enclosed in a with statement::
@@ -35,8 +35,14 @@ def query_count(desc=None):
     yield
     elapsed = time.time() - start_time
 
+    qc_diff = len(connection.queries) - original_query_count
+
     print >>sys.stderr, "%s: %d queries in %0.2f seconds" % (
         desc,
-        len(connection.queries) - original_query_count,
+        qc_diff,
         elapsed
     )
+
+    if print_queries:
+        for i in connection.queries[:-qc_diff]:
+            print >>sys.stderr, "\t%(time)s\t%(sql)s" % i
