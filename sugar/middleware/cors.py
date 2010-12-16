388dc724c2616afa17ffcfd903968fe41462f4d0
Chris Adams <chris@improbable.org>
2010-12-16 17:18:31 -0500

    CORS Middleware: more granular configuration
    Now you can control the CORS middleware using settings.CORS_PATH to
    specify a list of paths, mime types and headers to set for arbitrary parts of
    your site.
    
    This actually allows you to specify any header: perhaps this should be renamed
    to reflect its ability to function as an arbitrary HTTP header setter?

--- sugar/middleware/cors.py
+++ sugar/middleware/cors.py
@@ -1,42 +1,52 @@
 from django.conf import settings
 
+#: By default we'll set CORS Allow Origin * for all application/json responses
+DEFAULT_CORS_PATHS = (
+    ('/', ('application/json', ), (('Access-Control-Allow-Origin', '*'), )),
+)
+
 class CORSMiddleware(object):
     """
     Middleware that serves up representations with a CORS header to
-    allow third parties to use your web api from JavaScript without 
+    allow third parties to use your web api from JavaScript without
     requiring them to proxy it.
+
     See: http://www.w3.org/TR/cors/
 
-    Implement
-    ---------
-    Add to your middleware:
+    Installation
+    ------------
 
-    'sugar.middleware.cors.CORSMiddleware',
+    1. Add to ``settings.MIDDLEWARE_CLASSES``::
 
-    This will inject all your 'application/json' responses with:
+        'sugar.middleware.cors.CORSMiddleware',
 
-        Access-Control-Allow-Origin: *
+    2. Optionally, configure ``settings.CORS_PATHS`` if the default settings
+        aren't appropriate for your application. ``CORS_PATHS`` should be a
+        list of (path, content_types, headers) values where content_types and
+        headers are lists of mime types and (key, value) pairs, respectively.
 
-    Optionally you can configure the mediatypes you'd like to have be served 
-    up with CORS in your settings:
+        Processing occurs first to last so you should order ``CORS_PATHS``
+        items from most to least specific.
 
-    CORS = [ 
-            "{
-                ["application/json", "application/x-suggestions+json"],
-                "Access-Control-Allow-Origin": "*" 
-            }
-           ]
-           hmmm... not sure what to do here (yet)
+        See ``DEFAULT_CORS_PATHS`` for an example.
 
+    Notes
+    -----
+
+    * Although not officially a feature, the headers are not restricted to
+      the CORS spec and could conceivably include other values such as
+      desired (allowing, for example, custom ``Cache-Control`` settings).
     """
 
     def __init__(self):
-        # should any others be added here?
-        self.json_mediatypes = ["application/json"]
+        self.paths = getattr(settings, "CORS_PATHS", DEFAULT_CORS_PATHS)
 
     def process_response(self, request, response):
-        # just get the mediatype (strip charset)
-        mediatype = response.get('content-type', '').split(";")[0].lower()
-        if mediatype in self.json_mediatypes:
-            response['Access-Control-Allow-Origin'] = '*'
+        content_type = response.get('content-type', '').split(";")[0].lower()
+
+        for path, types, headers in self.paths:
+            if request.path.startswith(path) and content_type in types:
+                for k, v in headers:
+                    response[k] = v
+                break
         return response
--- sugar/tests.py
+++ sugar/tests.py
@@ -1,5 +1,6 @@
 import re
 
+from django.conf import settings
 from django.test.testcases import TestCase
 from django.http import HttpRequest, HttpResponse
 
@@ -47,9 +48,13 @@ class CORSTests(TestCase):
     def test_middleware(self):
         cors = CORSMiddleware()
         request = HttpRequest()
+        request.path = "/"
+
         response = HttpResponse('["foo"]',
                                 mimetype='application/json')
+
         cors.process_response(request, response)
+
         self.assertEqual(response['access-control-allow-origin'], '*')
 
     def test_non_interference(self):
@@ -57,9 +62,39 @@ class CORSTests(TestCase):
         cors = CORSMiddleware()
 
         request = HttpRequest()
+        request.path = "/cicero"
+
         response = HttpResponse('Lorem ipsum dolor sit amet',
                                 mimetype='text/html')
 
         cors.process_response(request, response)
 
         self.assertFalse(response.has_header('access-control-allow-origin'))
+
+    def test_custom_settings(self):
+        "CORS Middleware shouldn't touch responses outside of its mimetypes"
+
+        settings.CORS_PATHS = (
+            ('/foo', ('application/json', ), (('Access-Control-Allow-Origin', 'foo.example.com'), )),
+            ('/bar', ('application/json', ), (('Access-Control-Allow-Origin', 'example.com'), )),
+            ('/', ('application/json', ), (('Access-Control-Allow-Origin', '*'), )),
+        )
+
+
+        cors = CORSMiddleware()
+
+        request = HttpRequest()
+        request.path = "/test"
+
+        response = HttpResponse('["foo"]', mimetype='application/json')
+        cors.process_response(request, response)
+        self.assertEqual(response['access-control-allow-origin'], '*')
+
+        request.path = "/foo/bar/baaz/quux"
+        cors.process_response(request, response)
+        self.assertEqual(response['access-control-allow-origin'], 'foo.example.com')
+
+        request.path = "/bar/baaz/quux"
+        cors.process_response(request, response)
+        self.assertEqual(response['access-control-allow-origin'], 'example.com')
+
